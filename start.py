4844cc1c4fec6bf0d03bd204217d50516a8a04c5
hslupo <hslupo@gmail.com>
2021-12-11 19:03:03 +0100

    Wetterversuch

--- start.py
+++ start.py
@@ -1,1 +1,13 @@
-print("Hallo Welt")
+import requests
+
+API_KEY = "c3406cdf43ac617ac7ac96eaaa5bae3e"
+city = input("Hallo! Bitte gib eine Stadt ein: ")
+
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metricB&appid={API_KEY}'
+print(url)
+data = requests.get(url).json()
+print(data)
+temp = data['main']['temp']
+humidity = data['main']['humidity']
+
+print(f'In {city} beträgt die Temperatur {temp}. Die Luftfeuchtigkeit beträgt {humidity}%.')
