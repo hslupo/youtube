1cc84b5e7fe70e32b860732b9da72af722fd2bb9
hslupo <hslupo@gmail.com>
2021-12-14 10:56:30 +0100

    vom PC korrigiert

--- wetterdaten.py
+++ wetterdaten.py
@@ -3,11 +3,11 @@ import requests
 API_KEY = "c3406cdf43ac617ac7ac96eaaa5bae3e"
 city = input("Hallo! Bitte gib eine Stadt ein: ")
 
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metricB&appid={API_KEY}'
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
 print(url)
 data = requests.get(url).json()
 print(data)
 temp = data['main']['temp']
 humidity = data['main']['humidity']
 
-print(f'In {city} beträgt die Temperatur {temp}. Die Luftfeuchtigkeit beträgt {humidity}%.')
+print(f'In {city} beträgt die Temperatur {temp}°C. Die Luftfeuchtigkeit beträgt {humidity}%.')
