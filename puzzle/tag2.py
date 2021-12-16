0b5295bfc7cd01db3b441c87165ac64411af9fe6
hslupo <hslupo@gmail.com>
2021-12-16 11:47:04 +0100

    Puzzle 1-2

---
+++ puzzle/tag2.py
@@ -0,0 +1,15 @@
+    1-3 a: abcde
+    1-3 b: cdefg
+    2-9 c: ccccccccc
+
+    Jede Zeile enthält die Kennwortrichtlinie und dann das Kennwort.
+    Die Kennwortrichtlinie gibt an, wie oft ein bestimmter Buchstabe mindestens und höchstens vorkommen muss,
+    damit das Kennwort gültig ist. Zum Beispiel bedeutet 1-3 a,
+    dass das Kennwort mindestens 1 Mal und höchstens 3 Mal ein a enthalten muss.
+
+    In dem obigen Beispiel sind 2 Kennwörter gültig.
+    Das mittlere Kennwort,
+    cdefg, ist nicht gültig; es enthält keine Instanzen von b,
+    benötigt aber mindestens 1. Das erste und das dritte Kennwort sind gültig:
+    Sie enthalten ein a oder neun c,
+    beide innerhalb der Grenzen ihrer jeweiligen Richtlinien.
