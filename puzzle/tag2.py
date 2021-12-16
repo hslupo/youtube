76be25c272e9e654b3a4fa8919ca6702c1b8eed4
hslupo <hslupo@gmail.com>
2021-12-16 14:53:31 +0100

    vom PC korrigiert

--- puzzle/tag2.py
+++ puzzle/tag2.py
@@ -1,4 +1,4 @@
-    1-3 a: abcde
+"""    1-3 a: abcde
     1-3 b: cdefg
     2-9 c: ccccccccc
 
@@ -8,8 +8,15 @@
     dass das Kennwort mindestens 1 Mal und höchstens 3 Mal ein a enthalten muss.
 
     In dem obigen Beispiel sind 2 Kennwörter gültig.
-    Das mittlere Kennwort,
-    cdefg, ist nicht gültig; es enthält keine Instanzen von b,
-    benötigt aber mindestens 1. Das erste und das dritte Kennwort sind gültig:
+    Das mittlere Kennwort, cdefg, ist nicht gültig;
+    es enthält keine Instanzen von b, benötigt aber mindestens 1.
+    Das erste und das dritte Kennwort sind gültig:
     Sie enthalten ein a oder neun c,
     beide innerhalb der Grenzen ihrer jeweiligen Richtlinien.
+"""
+zeilen = []
+
+von, bis,c, pw = "1-3 b: cdefg".replace("-"," ").replace(":","").split()
+zeilen.append((int(von),int(bis),c,pw))
+
+
